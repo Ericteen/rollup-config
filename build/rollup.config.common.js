8f0a5b90c75e100efe54e248a8ea2fec38223912
eric_cheng <chengteen@yeah.net>
2020-04-12 17:51:30 +0800

    feat: add commonjs & node-resolve

--- build/rollup.config.common.js
+++ build/rollup.config.common.js
@@ -1,5 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'src/main.js',
@@ -12,6 +13,7 @@ export default {
   ],
   plugins: [
     resolve(),
+    commonjs(),
     babel({
       exclude: 'node_modules/**',
       "presets": [
